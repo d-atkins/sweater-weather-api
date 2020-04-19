4e3883eba957e97746832e21b2baab3a81e7e888
David Atkins <atki1080@gmail.com>
2020-04-19 13:24:25 -0600

    Adjust test

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe GeocodeService do
   describe 'methods' do
     it 'get_coordinates' do
 
-      address_data = GeocodeService.get_coordinates('denver,co')['data']['address_components']["formatted_address"]
+      address_data = GeocodeService.get_coordinates('denver,co')
 
-      expect(address_data['location']['lat']).to eq(39.91424689999999)
-      expect(address_data['location']['lng']).to eq(-104.6002959)
+      expect(address_data[:location][:lat]).to eq(39.7392358)
+      expect(address_data[:location][:lng]).to eq(-104.990251)
     end
   end
 end
