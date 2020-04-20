9eda5878a8d6e01be6186e08a9ab55bf746f998c
David Atkins <atki1080@gmail.com>
2020-04-20 10:40:42 -0600

    Make weather assertions less specific
    to allow tests to pass when weather changes

--- spec/requests/antipode/index_spec.rb
+++ spec/requests/antipode/index_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Antipode API',type: :request do
   it 'sends antipode info' do
-    # antipode_data = File.read('./spec/fixtures/antipode_response.json')
-    # antipode_response = double("response", status: 200, body: antipode_data)
-    #
-    # allow_any_instance_of(Faraday::Connection).to receive(:get).with('/data/2.5/onecall').and_return(antipode_response)
-
     get '/api/v1/antipode?location=Hong Kong'
 
     expect(response).to be_successful
@@ -16,8 +11,9 @@ RSpec.describe 'Antipode API',type: :request do
     expect(antipode_info[:id]).to be_nil
     expect(antipode_info[:type]).to eq('antipode')
     expect(antipode_info[:attributes][:location_name]).to eq('RP69, Jujuy, Argentina')
-    expect(antipode_info[:attributes][:forecast][:summary]).to eq('Clear Sky')
-    expect(antipode_info[:attributes][:forecast][:current_temperature]).to eq(54)
+    expect(antipode_info[:attributes][:forecast][:summary]).to_not be_empty
+    expect(antipode_info[:attributes][:forecast][:summary]).to be_instance_of(String)
+    expect(antipode_info[:attributes][:forecast][:current_temperature]).to be_instance_of(Integer)
     expect(antipode_info[:attributes][:search_location]).to eq('Hong Kong')
   end
 end
