0f8c4ab9f59c14cffc5580e85ac0c284a9117e3c
David Atkins <atki1080@gmail.com>
2020-04-20 10:02:45 -0600

    Add get_location

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -7,6 +7,14 @@ class GeocodeService
     JSON.parse(response.body, symbolize_names: true)[:results].first
   end
 
+  def self.get_location(lat, long)
+    response = conn.get('/maps/api/geocode/json') do |f|
+      f.params['key'] = ENV['GEOCODE_API_KEY']
+      f.params['latlng'] = "#{lat},#{long}"
+    end
+    JSON.parse(response.body, symbolize_names: true)[:results]
+  end
+
   private
 
     def self.conn
