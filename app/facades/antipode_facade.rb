348f45cc1dd23627bc8f434daf8de1eddd064faa
David Atkins <atki1080@gmail.com>
2020-04-20 09:50:24 -0600

    Add forecast

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,4 +1,5 @@
 class AntipodeFacade
+  attr_reader :location_name, :forecast, :search_location
 
   def initialize(location)
     @location = location
@@ -10,7 +11,14 @@ class AntipodeFacade
   end
 
   def forecast
+    antipode_forecast = Hash.new
+    antipode_forecast[:summary] = antipode_weather[:weather].first[:description].titlecase
+    antipode_forecast[:current_temperature] = antipode_weather[:temp].round
+    antipode_forecast
+  end
 
+  def search_location
+    @location
   end
 
   private
@@ -21,5 +29,9 @@ class AntipodeFacade
 
     def antipode_coordinates
       @antipode_coordinates ||= AntipodeService.get_antipode_coordinates(origin_coordinates[:lat], origin_coordinates[:lng])[:attributes]
+    end
+
+    def antipode_weather
+      @antipode_weather ||= OpenWeatherService.get_weather_data(antipode_coordinates[:lat], antipode_coordinates[:long])[:current]
     end
 end
