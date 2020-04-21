b23f1f9d8e12b8da66f987b59b865c70b977b873
David Atkins <atki1080@gmail.com>
2020-04-20 23:54:42 -0600

    Add RoadTripController

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::RoadTripController < ApplicationController
+  def create
+    user = User.find_by(api_key: user_params[:api_key])
+    if user
+      road_trip_facade = RoadTripFacade.new
+      render json: RoadTripSerializer.new(road_trip_facade)
+    else
+      render json: error, status: 401
+    end
+  end
+
+  private
+
+    def user_params
+      params.permit(:api_key)
+    end
+
+    def error
+      {Status: 401, Error: 'Unauthorized'}
+    end
+end
