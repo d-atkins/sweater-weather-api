4ca7dd3405a4e52204b202bc8e81ee18edccc6b2
David Atkins <atki1080@gmail.com>
2020-04-20 21:57:10 -0600

    Fix name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: UserSerializer.new(user), status: 201
+      render json: UsersSerializer.new(user), status: 201
     else
       render json: error(user), status: 400
     end
