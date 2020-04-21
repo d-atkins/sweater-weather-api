f118354497e8ad531137437eda76e61d5f95ff21
David Atkins <atki1080@gmail.com>
2020-04-20 18:53:02 -0600

    Add id

--- app/poros/background.rb
+++ app/poros/background.rb
@@ -1,5 +1,5 @@
 class Background
-  attr_reader :url_l, :url_o, :title
+  attr_reader :id, :url_l, :url_o, :title
   def initialize(image_info)
     @title = image_info[:title]
     @url_l = image_info[:url_l]
