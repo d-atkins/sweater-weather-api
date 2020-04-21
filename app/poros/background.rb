787e0dfd857837051b8c2e5020031df7ee654464
David Atkins <atki1080@gmail.com>
2020-04-20 18:43:08 -0600

    Add background poro

---
+++ app/poros/background.rb
@@ -0,0 +1,8 @@
+class Background
+  attr_reader :url_l, :url_o, :title
+  def initialize(image_info)
+    @title = image_info[:title]
+    @url_l = image_info[:url_l]
+    @url_o = image_info[:url_o]
+  end
+end
