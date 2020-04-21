ccc8d672c379b972e4731aad874b05da93d7ed26
David Atkins <atki1080@gmail.com>
2020-04-20 21:29:33 -0600

    Update user create spec

--- spec/requests/users/create_spec.rb
+++ spec/requests/users/create_spec.rb
@@ -3,8 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
   it 'creates new user' do
     registration_info = {email: 'unagi@example.com', password: 'burgers', password_confirmation: 'burgers'}
-    post '/api/v1/users?location=denver,co', params: registration_info
-    require "pry"; binding.pry
+    post '/api/v1/users',
+      params: registration_info.to_json,
+      headers: {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+
+    user = User.last
+
     expect(response).to be_successful
+
+    expect(user.api_key).to be_instance_of(String)
+    expect(user.api_key).to_not be_empty
+    expect(user.email).to eq('unagi@example.com')
+    expect(user.password_digest).to be_instance_of(String)
+    expect(user.password_digest).to_not be_empty
   end
 end
