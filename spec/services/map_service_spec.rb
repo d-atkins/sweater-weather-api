b97f5235078299039c568f25d6cd304e9c96d9e1
David Atkins <atki1080@gmail.com>
2020-04-20 23:30:54 -0600

    Add map service spec

---
+++ spec/services/map_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe MapService do
+  describe 'methods' do
+    it 'get_travel_time', :vcr do
+      travel_time = MapService.get_travel_time('denver,co', 'boulder,co')
+
+      expect(travel_time).to_not be_nil
+      expect(travel_time).to be_instance_of(String)
+      expect(travel_time.split.last).to eq('mins')
+    end
+  end
+end
