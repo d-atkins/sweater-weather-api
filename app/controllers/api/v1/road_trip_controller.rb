286f5baeac179d43db2a53066af5755ca715427c
David Atkins <atki1080@gmail.com>
2020-04-21 00:46:18 -0600

    Pass arguments

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: user_params[:api_key])
     if user
-      road_trip_facade = RoadTripFacade.new
+      road_trip_facade = RoadTripFacade.new(params[:origin], params[:destination])
       render json: RoadTripSerializer.new(road_trip_facade)
     else
       render json: error, status: 401
