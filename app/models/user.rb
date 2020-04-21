67d56c72935c205298b10be9d440b038bb6cae7b
David Atkins <atki1080@gmail.com>
2020-04-20 21:27:00 -0600

    Remove api_key validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :api_key, uniqueness: true, presence: true
-  validates :password, confirmation: true
+  validates :api_key, uniqueness: true
 
   has_secure_password
   has_secure_token :api_key
