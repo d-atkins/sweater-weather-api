c6817006831a83e4ba1bd75f988dcc69034ac30e
David Atkins <atki1080@gmail.com>
2020-04-20 21:39:04 -0600

    Validate password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :api_key, uniqueness: true
+  validates_presence_of :password_confirmation
 
   has_secure_password
   has_secure_token :api_key
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of :email}
     it {should validate_presence_of :password}
+    it {should validate_presence_of :password_confirmation}
     it {should validate_uniqueness_of :api_key}
     it {should validate_confirmation_of :password}
   end
